2b30f0c6fa9243865cb2efad2bc5eac1c3b25869
Florian Utku <utflorian@gmail.com>
2018-08-31 09:36:14 +0200

    color change with mock data

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-/* global $ */
+const rp = require('request-promise-native');
+const $ = require('jquery');
+
+const albertinaAuslastung = {
+  uri: 'http://localhost:3000/posts/albertina_auslastung',
+  headers: {
+    'User-Agent': 'Request-Promise',
+  },
+  json: true,
+};
 
 $(() => {
   $('.ampel').click((event) => {
@@ -20,4 +29,31 @@ $(() => {
       event.stopPropagation();
     }
   });
+
+  function setColor(id, value) {
+    if (value >= 0 && value <= 40) {
+      $(`#${id}`).attr('src', 'images/ampeln/ampel_gruen.jpg');
+      return;
+    }
+    if (value >= 41 && value <= 80) {
+      $(`#${id}`).attr('src', 'images/ampeln/ampel_gelb.jpg');
+      return;
+    }
+    $(`#${id}`).attr('src', 'images/ampeln/ampel_rot.jpg');
+  }
+
+  rp(albertinaAuslastung)
+    .then((response) => {
+      let i = 0;
+      let sum = 0;
+      Object.keys(response.orte).forEach((key) => {
+        i += 1;
+        sum += response.orte[key];
+        setColor(key, response.orte[key]);
+      });
+      setColor(response.name, Math.round(sum / i));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
